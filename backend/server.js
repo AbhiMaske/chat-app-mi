c4b2d60b09ae513c8604b2e7d6d6cc7d0d4215af
Abhay Maske <109457362+AbhiMaske@users.noreply.github.com>
2024-03-29 19:54:14 +0530

    all

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
 	// res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-	res.sendFile(path.join(process.cwd(),'client','dist','index.html'));
+	res.sendFile(path.join(__dirname,'client','dist','index.html'));
 });
 
 server.listen(PORT, () => {
